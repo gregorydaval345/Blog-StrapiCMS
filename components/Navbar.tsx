fda75b7e81274bb3180ab98c5b8e4a48fc46189d
gregorydaval345 <davalgregory69@gmail.com>
2022-12-12 12:58:03 +0000

    Initial Commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='flex items-center justify-between py-6'>
+            <Link href="/">
+                <div className='flex items-center'>
+                    {/* <Image src="" height={35} width={} /> */}
+                    <span className='font-bold ml-2 text-primary'>
+                        DG's Blog
+                    </span>
+                </div>
+            </Link>
+            <ul className="flex items-center">
+                <li className="mr-6 font-medium text-gray-600">
+                    <a href="#">Products</a>
+                </li>
+                <li className="mr-6 font-medium text-gray-600">
+                    <a href="#">Pricing</a>
+                </li>
+                <li className="mr-6 font-medium text-gray-600">
+                    <a href="#">Docs</a>
+                </li>
+                <li className="mr-6 font-medium text-gray-600">
+                    <a href="#">Company</a>
+                </li>
+            </ul>
+            <ul className="flex items-center">
+                <li className="mr-6 font-medium text-gray-600">
+                    <a href="#" className="hover:text-gray-400">
+                        Log in
+                    </a>
+                </li>
+                <li className="font-medium text-gray-600">
+                    <a
+                        href="#"
+                        className="bg-primary py-2 px-4 rounded-sm text-white hover:bg-primary-dark transition-all">
+                        Sign up
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
